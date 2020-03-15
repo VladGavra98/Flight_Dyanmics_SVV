dc8bba60eaa8b47dc0f80a0086cede806ce5f045
Florina Sirghi <f.sirghi-1@student.tudelft.nl>
2020-03-15 16:33:01 +0100

    Added the reference data in SI

--- unit_conversion.py
+++ unit_conversion.py
@@ -17,7 +17,7 @@ import os
 def conversion(filename):
 
     
-    
+    #c=0
     f = open(filename, "r")
     n = open(filename[:-4]+"SI.txt", "w")
     lines = f.readlines()
@@ -28,45 +28,54 @@ def conversion(filename):
         n.write('m/s^2\n')
         for i in range(len(lines)-2):
             n.write(str(float(lines[i+2].strip())*9.80665)+"\n")
+            #c += 1
     if unit == 'lbs\n':
         n.write('kg\n')
         for i in range(len(lines)-2):
             n.write(str(float(lines[i+2].strip())*0.45359237)+"\n")
+            #c += 1
     if unit == 'knots\n':
         n.write('m/s\n')
         for i in range(len(lines)-2):
             n.write(str(float(lines[i+2].strip())*0.5144444444444444)+"\n")
+            #c += 1
     if unit == 'ft\n':
         n.write('m\n')
         for i in range(len(lines)-2):
             n.write(str(float(lines[i+2].strip())*0.3048)+"\n")
+            #c += 1
     if unit == 'ft/min\n':
         n.write('m/s\n')
         for i in range(len(lines)-2):
             n.write(str(float(lines[i+2].strip())*0.00508)+"\n")
+            #c += 1
     if unit == 'deg C\n':
         n.write('deg K\n')
         for i in range(len(lines)-2):
             n.write(str(float(lines[i+2].strip())+ 273.15)+"\n")
+            #c += 1
     if unit == 'lbs/hr\n':
         n.write('kg/s\n')
         for i in range(len(lines)-2):
             n.write(str(float(lines[i+2].strip())*0.0001259979)+"\n")
+            #c += 1
     if unit == 'psi\n':
         n.write('Pa\n')
         for i in range(len(lines) - 2):
             n.write(str(float(lines[i + 2].strip())*6894.75729)+"\n")
+            #c += 1
     if unit!="knots\n" and unit!="lbs\n" and unit!="g\n" and unit!="ft\n" and unit!="ft/min\n" and unit!="deg C\n" and unit!="lbs/hr\n" and unit!="psi\n":
         for i in range(len(lines)-1):
             n.write(str(lines[i+1]))
-
+            #c += 1
+    #print(c)
     n.close()
 
-for filename in os.listdir('Data_new'):
-    name = "Data_new\\" + filename
+for filename in os.listdir('Reference_Data'):
+    name = "Reference_Data\\" + filename
     if "SI" in filename:
         os.remove(name)
 
-for filename in os.listdir('Data_new'):
-    name = "Data_new\\" + filename
+for filename in os.listdir('Reference_Data'):
+    name = "Reference_Data\\" + filename
     conversion(name)
