eb5b1424359f794c9881014bcf5ca1a4a60fb714
Phillipe <lothaller.phillipe@gmail.com>
2020-03-17 14:56:55 +0100

    Updated main file

--- testing.py
+++ testing.py
@@ -10,6 +10,7 @@ class TestStateSpace(unittest.TestCase):
     def test_system_eigenvalues(self):
 
         poles_a, A_a, poles_s, A_s = Cit_par.main(0)
+        print(A_a)
         assert all(poles_a) == all(np.linalg.eig(A_a)[0])
         assert all(poles_s) == all(np.linalg.eig(A_s)[0])
 
