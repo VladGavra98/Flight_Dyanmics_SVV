6e50747bb0ec6698c7bddafe9277d3cd60416ec1
maxrob27 <44088162+maxrob27@users.noreply.github.com>
2020-03-21 17:45:40 +0000

    changes

--- massbalance_old.py
+++ massbalance_old.py
@@ -9,9 +9,9 @@ blockfuel = 2640 #Fuel at t=0      check consistent with others
 #fuelusedlst = np.array([666, 555, 444, 333, 222, 111])  # Wf (Fuel Used)                USE OUR DATA
 #timelst = np.array([1, 2, 3, 4, 5, 6])  # t values for Wf (Fuel Used)   USE OUR DATA
 
-timelst = np.genfromtxt("Data_SI_correct/timeSI.txt",skip_header=2)
-lhfusi = np.genfromtxt("Data_SI_correct/lh_engine_FUSI.txt",skip_header=2)
-rhfusi = np.genfromtxt("Data_SI_correct/rh_engine_FUSI.txt",skip_header=2)
+timelst = np.genfromtxt("timeSI.txt",skip_header=2)
+lhfusi = np.genfromtxt("lh_engine_FUSI.txt",skip_header=2)
+rhfusi = np.genfromtxt("rh_engine_FUSI.txt",skip_header=2)
 fuelusedlst = (lhfusi+rhfusi)/0.453592   #given in SI and converted for consistency
 
 
@@ -39,8 +39,7 @@ fuelmomentall = 100*np.array([298.16,591.18,879.08,1165.42,1448.4,1732.53,2014.8
 
 #print(len(fuelmass),len(fuelmomentall)) #verification
 
-foo = 1  # Fuel on (1) or off (0)
-soo = 1  # Seats on (1) or off (0)
+
 
 
 def interpolate(x1,x2,y1,y2,x):
@@ -97,13 +96,11 @@ def cg(t,cgmove): #cgmove = False if Jack is in seat 8, set cgmove = True if Jac
         seatmoment += seats[s]*seatsarm[s]
         seatmass += seats[s]
 
-
-
-    momentsum = soo*seatmoment + BEW*BEWarm + foo*fuelmoment(t)
-    totalmass = soo*seatmass + BEW + foo*fuelonboard(t)
+    momentsum = seatmoment + BEW*BEWarm + fuelmoment(t)
+    totalmass = seatmass + BEW + fuelonboard(t)
     x_cg_inch = momentsum/totalmass
     x_cg = (x_cg_inch - 261.56)*2.54/100  #x_cg from LEMAC   in [m] !!
-    return x_cg #This is in [m] from LEMAC !! (not as frac/MAC)
+    return x_cg #This is in [m] from LEMAC !!
 
 def mass(t):
     seat1 = Arun
@@ -124,25 +121,24 @@ def mass(t):
     for s in range(len(seats)):
         seatmass += seats[s]
 
-    totalmass = soo*seatmass + BEW + foo*fuelonboard(t)
+    totalmass = seatmass + BEW + fuelonboard(t)
     return totalmass*0.453592 #Total mass in [kg] !!!
 
+print(cg(3000,True)-cg(3000,False))
 
-#print(cg(52.5*60,False)-cg(52.5*60,True))
-#print(100*(cg(10,False)*39.3701+261.56-291.65)/291.65,mass(10)/0.453592)
-xt = np.array([1358,1520,1682,1800,1890,2130,2450,2555,2652,2748,2805,2855,2970,3220,3435,3519,3610,3760]) #time list, note: no cg shift
+x = np.linspace(10,timelst[-1],1000)
+y = []
+for i in range(len(x)):
+    #y.append(cg(x[i],False))
+    y.append(mass(x[i]))
+
+# plt.plot(x,y)
+# plt.grid(True)
+# plt.xlabel('Time [s]')
+# #plt.ylabel('cg location/MAC [-]')
+# plt.ylabel('Mass [kg')
+# plt.show()
+
+#print(fuelmomentall(10))
 
 
-# #finding Report Requirements Bullet nr 1:
-# for l in xt:
-#     print(round((cg(l,False)*100/2.54+261.56),3))
-#
-# for l in xt:
-#     print(round((cg(l,False)),4))
-#
-# for l in xt:
-#     print(round(mass(l),3))
-#
-# #cg shift
-# print(round((cg(3157,True)*100/2.54+261.56),3),round((cg(3157,True)),3),round(mass(3157),3))
-# print(round(cg(3157,True),4))
