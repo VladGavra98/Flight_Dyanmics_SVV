3c615b16b839a3258c014555c557d4a974456882
maxrob27 <44088162+maxrob27@users.noreply.github.com>
2020-03-22 23:19:30 +0000

    nice

--- Test.py
+++ Test.py
@@ -1,11 +1,9 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
-
-def getInput(tab,timetab,t0,deltat): #to be exported; will be used for output as well
-    """Returns the sliced array from tab when
-    the time values (in seconds!) are contained in the interval (t0,t0+deltat)
-    Output: tab[slices],time[slices]"""
-
-    return tab[np.where((t0+deltat>timetab) & (t0<timetab))], timetab[np.where((t0+deltat>timetab) & (t0<timetab))]
-
-print(getInput(np.array([3,4,2,1,2]),np.array([1,2,3,4,5]),2.3,3))
+timelst = np.genfromtxt("Data_SI_correct/timeSI.txt",skip_header=2)
+rud = np.genfromtxt("Data_SI_correct/delta_rSI.txt",skip_header=2)
+plt.grid()
+plt.plot(timelst,rud,'r')
+plt.axvline(60.1*60+1.5)
+plt.show()
